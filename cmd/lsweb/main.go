6f7048649f9af78741c68a76c3cbeae26e63db19
elad el <elad140@gmail.com>
2023-08-13 17:03:06 +0300

    progressbar+gh

--- cmd/lsweb/main.go
+++ cmd/lsweb/main.go
@@ -5,6 +5,8 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"strings"
 
 	"github.com/hemzaz/lsweb/pkg/downloader"
 	"github.com/hemzaz/lsweb/pkg/parser"
@@ -17,22 +19,81 @@ var (
 	simFlag      bool
 	outputFlag   string
 	fileFlag     string
+	ghFlag       bool
 )
 
 func init() {
-	flag.StringVar(&urlFlag, "url", "", "The target URL to fetch and list/download files from.")
-	flag.BoolVar(&listFlag, "L", false, "List downloadable links. Default action when no flag is provided.")
-	flag.BoolVar(&downloadFlag, "D", false, "Download the files. Default to non-simultaneously.")
-	flag.BoolVar(&simFlag, "S", false, "Download simultaneously. Use with -D flag.")
-	flag.StringVar(&outputFlag, "O", "text", "Output format. Defaults to text. Options: json, txt, num, html.")
-	flag.StringVar(&fileFlag, "F", "", "File to write the output to.")
+	flag.StringVar(&urlFlag, "url", "", "the URL we are targeting")
+	flag.BoolVar(&listFlag, "L", false, "list downloadable links")
+	flag.BoolVar(&downloadFlag, "D", false, "download the files")
+	flag.BoolVar(&simFlag, "S", false, "download simultaneously")
+	flag.StringVar(&outputFlag, "O", "txt", "output format")
+	flag.StringVar(&fileFlag, "F", "", "file to write the output")
+	flag.BoolVar(&ghFlag, "gh", false, "fetch all releases from a GitHub URL")
+	flag.Parse()
+}
+
+func fetchGitHubReleases(repoURL string) ([]string, error) {
+	parts := strings.Split(repoURL, "/")
+	if len(parts) < 5 {
+		return nil, fmt.Errorf("invalid GitHub URL")
+	}
+	user, repo := parts[3], parts[4]
+
+	apiURL := fmt.Sprintf("https://api.github.com/repos/%s/%s/releases", user, repo)
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var releases []struct {
+		Assets []struct {
+			BrowserDownloadURL string `json:"browser_download_url"`
+		} `json:"assets"`
+	}
+
+	if err := json.Unmarshal(body, &releases); err != nil {
+		return nil, err
+	}
+
+	var downloadLinks []string
+	for _, release := range releases {
+		for _, asset := range release.Assets {
+			downloadLinks = append(downloadLinks, asset.BrowserDownloadURL)
+		}
+	}
+
+	return downloadLinks, nil
 }
 
 func main() {
-	flag.Parse()
+	if ghFlag {
+		links, err := fetchGitHubReleases(urlFlag)
+		if err != nil {
+			fmt.Println("Error fetching GitHub releases:", err)
+			return
+		}
 
-	if urlFlag == "" {
-		fmt.Println("Please provide a URL using the -url flag.")
+		if listFlag || (!listFlag && !downloadFlag) {
+			// Print the links
+			for _, link := range links {
+				fmt.Println(link)
+			}
+		}
+
+		if downloadFlag {
+			if simFlag {
+				downloader.DownloadFilesSimultaneously(links)
+			} else {
+				downloader.DownloadFiles(links, true)
+			}
+		}
 		return
 	}
 
@@ -43,63 +104,17 @@ func main() {
 	}
 
 	if listFlag || (!listFlag && !downloadFlag) {
-		displayLinks(links)
+		// Print the links
+		for _, link := range links {
+			fmt.Println(link)
+		}
 	}
 
 	if downloadFlag {
-		downloader.DownloadFiles(links, simFlag)
-	}
-}
-
-func displayLinks(links []string) {
-	switch outputFlag {
-	case "json":
-		data, err := json.Marshal(links)
-		if err != nil {
-			fmt.Println("Error marshaling links to JSON:", err)
-			return
-		}
-		fmt.Println(string(data))
-	case "txt":
-		for _, link := range links {
-			fmt.Println(link)
-		}
-	case "num":
-		for i, link := range links {
-			fmt.Printf("%d. %s\n", i+1, link)
-		}
-	case "html":
-		fmt.Println("<ul>")
-		for _, link := range links {
-			fmt.Printf("<li><a href='%s'>%s</a></li>\n", link, link)
-		}
-		fmt.Println("</ul>")
-	default:
-		for _, link := range links {
-			fmt.Println(link)
-		}
-	}
-
-	// If fileFlag is provided, write the output to the specified file
-	if fileFlag != "" {
-		var content string
-		switch outputFlag {
-		case "json":
-			data, err := json.Marshal(links)
-			if err != nil {
-				fmt.Println("Error marshaling links to JSON:", err)
-				return
-			}
-			content = string(data)
-		case "txt", "num", "html":
-			for _, link := range links {
-				content += link + "\n"
-			}
-		}
-		err := ioutil.WriteFile(fileFlag, []byte(content), 0644)
-		if err != nil {
-			fmt.Println("Error writing to file:", err)
-			return
+		if simFlag {
+			downloader.DownloadFilesSimultaneously(links)
+		} else {
+			downloader.DownloadFiles(links, true)
 		}
 	}
 }
--- pkg/downloader/downloader.go
+++ pkg/downloader/downloader.go
@@ -3,7 +3,6 @@ package downloader
 import (
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -12,82 +11,56 @@ import (
 	"github.com/schollz/progressbar/v3"
 )
 
-const MaxConcurrentDownloads = 255
-
-var errorsMutex sync.Mutex
-
-func DownloadFiles(urls []string, simultaneously bool) error {
-	if simultaneously {
-		return downloadFilesConcurrently(urls)
-	}
-	for _, url := range urls {
-		err := downloadFile(url)
-		if err != nil {
-			log.Printf("Failed to download %s: %v", url, err)
-			return fmt.Errorf("failed to download %s: %w", url, err)
-		}
-	}
-	return nil
-}
-
-func downloadFilesConcurrently(urls []string) error {
-	sem := make(chan struct{}, MaxConcurrentDownloads)
-	var wg sync.WaitGroup
-	var errors []error
-
-	for _, url := range urls {
-		wg.Add(1)
-		go func(u string) {
-			defer wg.Done()
-			sem <- struct{}{}
-			err := downloadFile(u)
-			if err != nil {
-				errorsMutex.Lock()
-				errors = append(errors, fmt.Errorf("failed to download %s concurrently: %w", u, err))
-				errorsMutex.Unlock()
-			}
-			<-sem
-		}(url)
-	}
-	wg.Wait()
-
-	if len(errors) > 0 {
-		for _, err := range errors {
-			log.Println(err)
-		}
-		return fmt.Errorf("multiple errors occurred during concurrent downloads: %v", errors[0]) // return the first error for simplicity
-	}
-	return nil
-}
-
-func downloadFile(url string) error {
+// DownloadFile downloads a file from the given URL and saves it to the current directory.
+func DownloadFile(url string, showProgress bool) error {
 	resp, err := http.Get(url)
 	if err != nil {
-		return fmt.Errorf("failed to fetch %s: %w", url, err)
+		return err
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("failed to download %s: HTTP %d", url, resp.StatusCode)
-	}
-
 	filename := filepath.Base(url)
-	if _, err := os.Stat(filename); !os.IsNotExist(err) {
-		filename = "new_" + filename
-	}
-	out, err := os.Create(filename)
+	file, err := os.Create(filename)
 	if err != nil {
-		return fmt.Errorf("failed to create file %s: %w", filename, err)
+		return err
 	}
-	defer out.Close()
+	defer file.Close()
 
-	bar := progressbar.DefaultBytes(
-		resp.ContentLength,
-		"downloading",
-	)
-	_, err = io.Copy(io.MultiWriter(out, bar), resp.Body)
-	if err != nil {
-		return fmt.Errorf("failed to write to file %s: %w", filename, err)
+	if showProgress {
+		bar := progressbar.DefaultBytes(
+			resp.ContentLength,
+			"downloading",
+		)
+		_, err = io.Copy(io.MultiWriter(file, bar), resp.Body)
+	} else {
+		_, err = io.Copy(file, resp.Body)
 	}
-	return nil
+
+	return err
+}
+
+// DownloadFiles downloads multiple files.
+func DownloadFiles(urls []string, showProgress bool) {
+	for _, url := range urls {
+		err := DownloadFile(url, showProgress)
+		if err != nil {
+			fmt.Println("Error downloading file:", err)
+		}
+	}
+}
+
+// DownloadFilesSimultaneously downloads multiple files in parallel.
+func DownloadFilesSimultaneously(urls []string) {
+	var wg sync.WaitGroup
+	for _, url := range urls {
+		wg.Add(1)
+		go func(url string) {
+			defer wg.Done()
+			err := DownloadFile(url, true)
+			if err != nil {
+				fmt.Println("Error downloading file:", err)
+			}
+		}(url)
+	}
+	wg.Wait()
 }
